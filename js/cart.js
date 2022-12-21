ca9b14613934961627fb1802c95121d554acc71e
CorAurum <fabriciogarcia20@gmail.com>
2022-12-21 18:02:58 -0300

    Cuarto commit

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,26 @@
- 
+
+function EliminarProductosEnCarro(EliminarCarro) {
+
+    let UnidadesEnCarro = localStorage.getItem('ProductosEnCarro');
+    UnidadesEnCarro = JSON.parse(UnidadesEnCarro);
+  
+    if(UnidadesEnCarro != null) {
+  
+       if(UnidadesEnCarro[EliminarCarro.nombre] == undefined ) {
+  
+           UnidadesEnCarro = {
+            ...delete Productos[EliminarCarro],
+             [EliminarCarro.nombre]: EliminarCarro
+           }
+  
+       }
+           UnidadesEnCarro[EliminarCarro.nombre].CopiasEnCarro == 0;
+    } else { 
+       EliminarCarro.CopiasEnCarro = 1;
+       UnidadesEnCarro = {
+         [EliminarCarro.nombre]: EliminarCarro
+       }
+    }
+           localStorage.setItem("ProductosEnCarro", JSON.stringify(UnidadesEnCarro));
+           
+  }; 
--- js/main.js
+++ js/main.js
@@ -8,74 +8,62 @@ let Productos=[
     {
         nombre: 'All Systems Red',
         precio:100,
-        CopiasEnCarro:1
+        CopiasEnCarro:0,
+        imagenURL: 'All systems red - The murderbot diaries - Martha wells.jpg'
     },
 
     {
         nombre:'Kings of the wyld',
         precio:200,
-        CopiasEnCarro:1
+        CopiasEnCarro:0,
+        imagenURL: 'Kings of the wyld - Nicholas eames.jpg'
     },
 
     {
         nombre:'Clockwork boys',
         precio:300,
-        CopiasEnCarro:1
+        CopiasEnCarro:0,
+        imagenURL: 'Clockwork boys - Clocktaur war - T.Kingfisher.jpg'
     },
 
     {
         nombre:'StormFront',
         precio:400,
-        CopiasEnCarro:1
+        CopiasEnCarro:0,
+        imagenURL: 'StormFront - Dresden files - Jim butcher.jpg'
     },
 
     {
         nombre:'The Way of Kings',
         precio:500,
-        CopiasEnCarro:1
+        CopiasEnCarro:0,
+        imagenURL: 'The Way of Kings - Stormlight Archive - Brandon Sanderson.jpg'
     },
 
     {
         nombre:'Warbreaker',
         precio:600,
-        CopiasEnCarro:1
+        CopiasEnCarro:0,
+        imagenURL: 'Warbreaker - Brandon sanderson.jpg'
     },
-]
-
-
+];
 
 
 for(let i=0;i<Carro.length;i++)
 {
-
     Carro[i].addEventListener('click', ()=>{
 
         CostoTotal(Productos[i]);
         CantidadCarro(Productos[i]);
-        ProductosEnCarro(Productos[i])
-        AgregarProductos(Productos[i]);
+        MostrarProductosEnCarro(Productos[i]);
+        console.log(Productos[i]);
        
     })    
 };
 
-/* Funcion que suma la cantidad de unidades elegida por el cliente del producto seleccionado*/
-
-function ProductosEnCarro(copias) {
-
-    let CopiasIndividualesEnCarro = localStorage.getItem('Copias '+copias.nombre);
-  
-    if(CopiasIndividualesEnCarro!=null){
-  
-        CopiasIndividualesEnCarro=parseInt(CopiasIndividualesEnCarro);
-          localStorage.setItem('Copias '+copias.nombre,CopiasIndividualesEnCarro+copias.CopiasEnCarro);
-    }else {
-      localStorage.setItem('Copias '+copias.nombre, copias.CopiasEnCarro);
-    }
-  
-  };
-
 
 /* Funcion que suma el valor total de todos los productos agregados al carro */  
+
 function CostoTotal(producto) {
 
   let CostoEnCarro=localStorage.getItem('CostoTotal');
@@ -98,9 +86,9 @@ function CantidadCarro(cantidad) {
   if(CantidadEnCarro!=null){
 
         CantidadEnCarro=parseInt(CantidadEnCarro);
-        localStorage.setItem('CantidadCarro',CantidadEnCarro+cantidad.CopiasEnCarro);
+        localStorage.setItem('CantidadCarro',CantidadEnCarro+1);
   }else {
-    localStorage.setItem('CantidadCarro', cantidad.CopiasEnCarro);
+    localStorage.setItem('CantidadCarro', CantidadEnCarro+1);
   }
 
   ProCont = window.localStorage.getItem('CantidadCarro'); 
@@ -110,19 +98,78 @@ function CantidadCarro(cantidad) {
 
 /* Funcion que agregar el producto a las variables locales */
 
-function AgregarProductos(agregar) {
+function MostrarProductosEnCarro(MostrarCarro) {
 
- localStorage.setItem('ProductoEnCarro ' +agregar.nombre, JSON.stringify(agregar));
+  let UnidadesEnCarro = localStorage.getItem('ProductosEnCarro');
+  UnidadesEnCarro = JSON.parse(UnidadesEnCarro);
 
+  if(UnidadesEnCarro != null) {
+
+     if(UnidadesEnCarro[MostrarCarro.nombre] == undefined ) {
+
+         UnidadesEnCarro = {
+           ...UnidadesEnCarro,
+           [MostrarCarro.nombre]: MostrarCarro
+         }
+
+     }
+         UnidadesEnCarro[MostrarCarro.nombre].CopiasEnCarro += 1;
+  } else { 
+     MostrarCarro.CopiasEnCarro = 1;
+     UnidadesEnCarro = {
+       [MostrarCarro.nombre]: MostrarCarro
+     }
+  }
+         localStorage.setItem("ProductosEnCarro", JSON.stringify(UnidadesEnCarro));
+         
+}; 
+
+/* muestra los productos en el carrito de compras */
+
+function MostrarEnCarro() {
+  let ItemsEnCarro = localStorage.getItem("ProductosEnCarro");
+  ItemsEnCarro = JSON.parse(ItemsEnCarro);
+  let ContenedorDeProductos = document.querySelector(".CarroProducto");
+
+  console.log(ItemsEnCarro);
+  if (ItemsEnCarro && ContenedorDeProductos) {
+
+    ContenedorDeProductos.innerHTML = '';
+    Object.values(ItemsEnCarro).map(item => {
+      
+
+      ContenedorDeProductos.innerHTML += `
+              
+              <button class="EliminarProductoCarro" onclick="BorrarProducto()">
+              <img src="../img/icons/delete.png">
+            </button>
+
+            <img src="../front-covers/${item.imagenURL}">
+
+            <p>
+              ${item.precio}
+            </p>
+
+            <div>
+              <button><img src="../img/icons/minus.png"></button>
+              <p>${item.CopiasEnCarro}</p>
+              <button class="AgregarUnidadJS"><img src="../img/icons/plus.png"></button>
+            </div>
+            <p class="CarroProductoTotal">
+            ${item.precio * item.CopiasEnCarro}
+            </p>  `;
+    });
+
+  } 
 };
 
 
-function MostrarProductosEnCarro(MostrarCarro) {
+/* Borra productos del carro */
 
-    let InventarioCarro = local
+function EliminarProductosEnCarro() {
+  console.log("Borrado");
 
-}
-
+}; 
 
 /*Funcion que muestra el numero de productos en el carrito de compras del NavBar*/
 
@@ -130,5 +177,5 @@ function MostrarCarro(){
 
     ProCont = window.localStorage.getItem('CantidadCarro'); 
     document.getElementById('CartCont').innerHTML = ProCont;
-
 };
+
