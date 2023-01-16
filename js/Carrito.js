f6a83b070676823ace0871da593be74bd05d8e98
CorAurum <fabriciogarcia20@gmail.com>
2023-01-16 17:51:48 -0300

    Diecisieteavo commit

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,5 +1,4 @@
 /* Funcion que muestra las unidades en el carrito de compras de la navbar en las distintas secciones de la pagina */
-
 function MostrarCarro(){
     ProCont = window.localStorage.getItem('CantidadCarro'); 
     document.getElementById('CartCont').innerHTML = ProCont;
