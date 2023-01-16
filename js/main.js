4dcef2384cb3bbd8ae49f9dfe156aff47707798a
CorAurum <fabriciogarcia20@gmail.com>
2023-01-16 17:47:26 -0300

    Dieciseisavo commit

--- js/main.js
+++ js/main.js
@@ -298,7 +298,6 @@ function MostrarTotal(){
   document.getElementById('CosteFinalTotal').innerHTML = TotalCosto+"$";
 };
 
-
 /* Funcion que muestra las unidades en el carrito de compras de la navbar en las distintas secciones de la pagina */
 
 function MostrarCarro(){
